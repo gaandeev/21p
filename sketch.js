79289b5be9e259e77ca562b1e96762091f1f60d4
gaandeev <77530799+gaandeev@users.noreply.github.com>
2021-02-14 07:44:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+   
+
+
+
+  
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+    edge=createEdgesprite();
+
+
+
+}
